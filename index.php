90aff73a690d70960575d12164053d147961393c
CODEHRS1110 <87331517+CODEHRS1110@users.noreply.github.com>
2021-07-13 21:26:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Home | Sparks Foundation Bank</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="stylesheet" href="CSS/footer.css">
+
+</head>
+<body>
+<header>
+  <!-- NAVBAR -->
+    <div class="container-fluid p-0">
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="index.php" style="font-weight: bold;">
+           <i class="fas fa-university fa-2x mx-3"></i>Sparks Foundation</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto text-right">
+            <li class="nav-item">
+              <a class="nav-link active-home" href="index.php" style="font-weight:bold;">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#foot" style="font-weight:bold;">ABOUT US</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <!-- CONTENT 1 -->
+    <div class="container text-center">
+      <div class="row">
+       <div class="img">
+           <img src="IMAGES/bank-building.png" >
+       </div>
+        <div class="col-md-7 col-sm-12  text-white">
+          <h1>SPARKS FOUNDATION BANK</h1>
+          <p>
+            Bank to Believe
+          </p>
+          <a href="#service"><button class="btn btn-light px-5 py-2 primary-btn">GET STARTED</button></a>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+        </div>
+     
+    </div>
+    </div>
+</header>
+<!-- SERVICES -->
+<section class="service" id="service">
+  <div class="container">
+   <br>
+   <br>
+    <h2 class="title">WE PROVIDE A WIDE RANGE<br> OF SERVICES</h2>
+    <p class="subtitle">Let's Start</p>
+      <div class="row offset-md-3">
+        <div class="col-md-4">
+          <a href="history.php" style="text-decoration:none; color:black;">
+            <div class="service-box">
+              <i class="fa fa-history fa-5x" style="margin-bottom:50px"></i>
+              <h6>TRANSACTION HISTORY</h6>
+              <p>Views the record of every transaction</p>
+              <i class="fa fa-arrow-right"></i>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a href="usertable.php" style="text-decoration:none; color:black;">
+            <div class="service-box">
+              <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+              <h6>VIEW CUSTOMERS</h6>
+              <p>View all the customers having account in our bank</p>
+              <i class="fa fa-arrow-right"></i>
+            </div>
+          </a>
+        </div>    
+      </div>
+  </div>
+</section>
+<br>
+<br>
+<br>
+<br>
+<!-- ABOUT US -->
+<footer id="foot">
+   <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download Our App</h3>
+                    <p>Download App for Android and IOS Mobile Phone.</p>
+                    <div class="app-logo">
+                        <img src="images/play-store.png">
+                        <img src="images/app-store.png">
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <img src="IMAGES/logo_small.png">
+                    <p>We understand your world</p>
+                </div>
+               
+                <div class="footer-col-4">
+                    <h3>Follow us</h3>
+                    <div class="icons">
+                    <a href="https://github.com/CODEHRS1110"><i class="fab fa-github" style="font-size:30px"></i></a>
+          <a href="https://www.instagram.com/himanshu.sachdev/"><i class="fab fa-instagram" style="margin-left:15px;font-size:30px"></i></a>
+          <a href="https://www.linkedin.com/in/himanshu-sachdev-a9b018112/" style="margin-left:15px;font-size:30px"><i class="fab fa-linkedin"></i></a>
+          </div>
+          </div>
+            </div>
+            <hr>
+            <p class="copyright">Copyright 2021-Himanshu Sachdev</p>
+        </div>
+    </div>
+ </footer>
+</body>
+</html>
